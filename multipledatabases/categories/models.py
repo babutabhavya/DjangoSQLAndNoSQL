284c8da671ee0a9e6f65643b9c88f5f0122b3839
Bhavya Babuta <babutabhavya@gmail.com>
2024-02-18 01:57:48 +0100

    Allow relations between nosql and sql

--- multipledatabases/categories/models.py
+++ multipledatabases/categories/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Category(models.Model):
     text = models.CharField(max_length=200)
 
+    def __str__(self) -> str:
+        return self.text
+
     class Meta:
         verbose_name_plural = "Categories"
